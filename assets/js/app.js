a3247e7635720780863c034cdfbfb245b1d4be73
Carolina <carolinacarrilr@gmail.com>
2018-06-26 21:47:42 -0400

    dos columnas y cuadro de texto

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+console.clear(); // Esto limpia la consola
+
+$('#uploader').submit(function(evt){
+  evt.preventDefault();
+  
+  // Create a reader
+  var reader = new FileReader();
+  
+  // Get the image
+  var file = $(evt.target).find('input[type="file"]').get(0).files[0];
+  
+  reader.readAsDataURL(file);
+  
+  reader.onload = function(e){
+    console.log('The image was load');
+    $('#image').attr('src', e.target.result);
+  };
+  
+}); 
